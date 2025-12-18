cf8492832300a2c39d64d30db601734518110226
Yvonne Marneth <marneth.yvonne@gmail.com>
2025-12-18 13:02:47 +0100

    Add koin Modules

--- JustGoUp/composeApp/src/commonMain/kotlin/org/clc/justgoup/di/DatabaseModule.kt
+++ JustGoUp/composeApp/src/commonMain/kotlin/org/clc/justgoup/di/DatabaseModule.kt
@@ -1,2 +1,11 @@
 package org.clc.justgoup.di
 
+import org.clc.justgoup.cache.Database
+import org.clc.justgoup.climbingSession.ClimbingSessionRepository
+import org.clc.justgoup.climbingSession.ClimbingSessionRepositoryImpl
+import org.koin.dsl.module
+
+val databaseModule = module {
+    single { Database(get()) }
+    single<ClimbingSessionRepository> { ClimbingSessionRepositoryImpl(get()) }
+}
---
+++ JustGoUp/composeApp/src/commonMain/kotlin/org/clc/justgoup/di/PlatformModule.kt
@@ -0,0 +1,5 @@
+package org.clc.justgoup.di
+
+import org.koin.core.module.Module
+
+expect val platformModule: Module
