e0f9801f7a8b9cf1c98134739d274c20a0c7b08b
Yvonne Marneth <marneth.yvonne@gmail.com>
2026-07-08 13:22:48 +0200

    Cleanup unnecessary project nesting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeMultiplatform) apply false
