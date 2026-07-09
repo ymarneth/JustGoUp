e1fdfb72bbacf5b96df859c3d4c098d568080674
Yvonne Marneth <marneth.yvonne@gmail.com>
2026-07-09 22:56:47 +0200

    Replace deprecated icons-extended with icons-core

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,7 +43,7 @@ kotlin {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
-            implementation(libs.compose.material.icons.extended)
+            implementation(libs.compose.material.icons.core)
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.ui.tooling.preview)
